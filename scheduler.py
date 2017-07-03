8f0ba45d33c7cf0542a872c10ee2fb6c1802dbbc
Satya Arjunan <satya.arjunan@gmail.com>
2017-07-03 15:35:19 +0900

    Added timing and multiple processes

--- scheduler.py
+++ scheduler.py
@@ -22,33 +22,37 @@ def step(running, c, a, e1, e2):
   while running.value:
     c.increment()
     e1.wait()
-    a[0] = 5
     c.increment()
     e2.wait()
 
-def scheduler_step(running, c, a, e1, e2):
-  while c.value() == 0:
+def scheduler_step(running, c, a, e1, e2, np):
+  while c.value() != np:
     pass
   e2.clear()
   c.reset()
   e1.set();
-  while c.value() == 0:
+  while c.value() != np:
     pass
-  print(c.value(), a[0])
+  #print(c.value(), a[0])
   e1.clear()
   c.reset()
   e2.set()
 
 if __name__ == '__main__':
+  n = 10000
+  np = 10
   c = Counter(0)
   running = multiprocessing.Value('i', 1)
   e1 = multiprocessing.Event()
   e2 = multiprocessing.Event()
   a = multiprocessing.Array('i', size)
-  p = multiprocessing.Process(target=step, args=(running, c, a, e1, e2))
-  p.start()
-  for i in range(10):
-    scheduler_step(running, c, a, e1, e2)
+  procs = [multiprocessing.Process(target=step, args=(running, c, a, e1, e2)) 
+          for i in range(np)]
+  for p in procs: p.start()
+  t0 = time.time()
+  for i in range(n):
+    scheduler_step(running, c, a, e1, e2, np)
   e1.set()
   running.value = 0;
+  print('Latency: ', (time.time() - t0) / (n*2*np) * 1E6, 'microseconds')
   p.join()
