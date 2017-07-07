b2b9681fd6c53b9740206b32a714e130efb16290
Satya Arjunan <satya.arjunan@gmail.com>
2017-07-07 16:50:16 +0900

    Added pool map scheduler

---
+++ scheduler_pool_map.py
@@ -0,0 +1,25 @@
+import multiprocessing as mp
+import my
+import numpy as np
+import time
+
+def testFunc(myid):
+  pass
+
+def initProcess(a):
+  my.a = a
+
+if __name__ == "__main__":
+  n = 100
+  pn = 10
+  a = mp.Array('i', pn, lock=False)
+  for i in range(pn):
+    a[i] = i
+  b = np.arange(pn)
+  p = mp.Pool(processes=pn, initializer=initProcess,initargs=(a,))
+  t0 = time.time()
+  for i in range(n):
+    p.map(testFunc, b)
+  p.close()
+  print('Latency: ', (time.time() - t0) / (n*2*pn) * 1E6, 'microseconds')
+  p.join()
