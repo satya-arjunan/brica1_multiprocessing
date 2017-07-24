fbb205d8efc8281777b9cbab126d7a231c31aa3f
Satya Arjunan <satya.arjunan@gmail.com>
2017-07-24 15:36:23 +0900

    Scheduler with events and timings

--- brica1/port.py
+++ brica1/port.py
@@ -34,14 +34,13 @@ class Port(object):
         """
 
         super(Port, self).__init__()
-        ##ct = numpy.ctypeslib.as_ctypes(value)
-        ##while not isinstance(ct, str):
-        ##  ct = ct._type_
-        #self.shared_buffer = multiprocessing.RawArray(ctypes.c_double,
-        #    len(value))
-        #self.buffer = numpy.frombuffer(self.shared_buffer)
-        #self.buffer[:] = value.copy()
-        self.buffer = value
+        #ct = numpy.ctypeslib.as_ctypes(value)
+        #while not isinstance(ct, str):
+        #  ct = ct._type_
+        self.shared_buffer = multiprocessing.RawArray(ctypes.c_double,
+            len(value))
+        self.buffer = numpy.frombuffer(self.shared_buffer)
+        self.buffer[:] = value.copy()
         self.callbacks = []
 
     def connect(self, target):
